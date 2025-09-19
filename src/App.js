ab097fac1482b70ed222424179954b84ab6afa37
Teja201920 <darlingteja2019@gmail.com>
2025-09-19 09:57:06 +0530

    code done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Greeting from "./Greeting";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Experiment 3: Props Example</h1>
+      {/* Passing data from parent to child */}
+      <Greeting name="Tejaswarrao" message="Welcome to learning React props!" />
+      <Greeting name="Centurion Blockchain Club" message="Happy coding with React!" />
     </div>
   );
 }
